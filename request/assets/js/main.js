95067972a8b40e3142e46181dca5dbc64b993446
igormaia01 <igormaia2001@gmail.com>
2022-06-07 19:28:12 -0500

    using fetch

--- request/assets/js/main.js
+++ request/assets/js/main.js
@@ -1,20 +1,19 @@
-const request = obj => {
-    return new Promise((resolve, reject) => {
-        console.log(resolve)
-        const xhr = new XMLHttpRequest();
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
 
-        xhr.open(obj.method, obj.url, true);
-        xhr.send();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
 
-        xhr.addEventListener('load', () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject(xhr.statusText);
-            }
-        });
-    })
-}
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.response);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     })
+// }
 
 document.addEventListener('click', e => {
     const target = e.target;
@@ -35,8 +34,10 @@ async function carregaPagina(el) {
         url: href,
     }
     try {
-        const response = await request(objConfig)
-        carregaResultado(response)
+        const response = await fetch(href)
+        if (response.status !== 200) throw new Error(response.statusText)
+        const html = await response.text()
+        carregaResultado(html)
     } catch (error) {
         console.log(error)
     }
@@ -46,3 +47,18 @@ function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 }
+
+fetch('pagina2.html')
+    .then(response => {
+        if (response.status !== 200) {
+            console.log(`Erro: ${response.status}`)
+            return
+        }
+        return response.text()
+    })
+    .then(text => {
+        console.log(text)
+    })
+    .catch(error => {
+        console.log(error)
+    })
